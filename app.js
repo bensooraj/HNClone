dfb4db081390293167eae4cc7f74d2e09af2d552
Ben Sooraj <bensoorajm@gmail.com>
2018-02-15 16:00:43 +0530

    Get and Post routes for Bensooraj

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 
 // Middlewares
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     console.log("Requested URL: " + req.url);
     next();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,35 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+var bodyParser = require("body-parser");
 
+// Middlewares
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
 router.get('/', function (req, res) {
     db.User.find({})
         .then(function (users) {
-            res.render('users/users', {users});
+            res.render('users/users', { users });
         }, function (err) {
             res.send("ERROR listing users @ " + req.url);
         });
 });
 
 router.post('/', function (req, res) {
-    db.User.find({})
-        .then(function (users) {
-            res.render('users', {users});
-        }, function (err) {
-            res.send("ERROR: " + err + ", listing users @ " + req.url);
-        });
+    console.log('POST data:');
+    console.log(req.body);
+    // db.User.find({})
+    //     .then(function (users) {
+    //         res.render('users', {users});
+    //     }, function (err) {
+    //         res.send("ERROR: " + err + ", listing users @ " + req.url);
+    //     });
+    db.User.create(req.body)
+        .then(function () {
+            res.redirect('/')
+        })
+    res.redirect('/');
 });
 
 
