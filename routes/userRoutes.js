fd27736369b172e955529cbf311073147642b75c
Ben Sooraj <bensoorajm@gmail.com>
2018-02-20 00:10:59 +0530

    fixed user's posts route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,10 @@ var postRoutes = require('./postRoutes');
 
 // Middlewares
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use('/:username/posts', postRoutes);
+// router.use('/:username/posts', postRoutes);
 
 // Routes
+// Overview | User's page
 router.get('/:username', async function (req, res) {
     var userProfile = await db.User
         .findOne({ username: req.params.username })
@@ -22,6 +23,23 @@ router.get('/:username', async function (req, res) {
         });
     } else {
         res.render('users/user', { userProfile });
+    }
+});
+
+// Posts | User's page
+router.get('/:username/posts', async function (req, res) {
+    var userProfile = await db.User
+        .findOne({ username: req.params.username })
+        .populate('posts')
+        // .populate('comments')
+        .exec();
+
+    if (!userProfile) {
+        res.render('users/noUser', {
+            username: req.params.username
+        });
+    } else {
+        res.render('users/posts', { userProfile });
     }
 });
 
