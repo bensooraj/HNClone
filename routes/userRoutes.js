4635b3028f8cb22e46a865d2f8cdb5459731fe90
Ben Sooraj <bensoorajm@gmail.com>
2018-02-19 23:58:06 +0530

    fixed user's overview view

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,31 +10,19 @@ router.use('/:username/posts', postRoutes);
 
 // Routes
 router.get('/:username', async function (req, res) {
-    var user = await db.User
+    var userProfile = await db.User
         .findOne({ username: req.params.username })
         .populate('posts')
         .populate('comments')
         .exec();
 
-    if (!user) {
+    if (!userProfile) {
         res.render('users/noUser', {
             username: req.params.username
         });
     } else {
-        res.render('users/user', { user });
+        res.render('users/user', { userProfile });
     }
-    // db.User.find({ username: req.params.username })
-    //     .then(function (user) {
-    //         console.log(user[0]);
-    //         if (!user[0]) {
-    //             console.log('The user you are looking for, doesn\'t exist!');
-    //             res.send('The user you are looking for, doesn\'t exist!');
-    //         } else {
-    //             res.render('users/user', { user });
-    //         }
-    //     }, function (err) {
-    //         res.send("ERROR: " + err + ", listing users @ " + req.url);
-    //     });
 });
 
 module.exports = router;
